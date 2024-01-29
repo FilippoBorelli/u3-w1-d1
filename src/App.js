05a533895f91026ec006d714127f9ed096f5ed3a
Filippo2803 <filippoborelli8@gmail.com>
2024-01-29 14:26:57 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonComponent from './Button';
+import ImageComponent from './Image';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ButtonComponent textToShow="Questo è un bottone"/>
+        <ImageComponent source="https://placekitten.com/300" alt="Foto gatti" />
         <a
           className="App-link"
           href="https://reactjs.org"
